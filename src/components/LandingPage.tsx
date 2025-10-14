fbe22488117f23678b6287f33f9f09ed2f678087
Ivan Boole <64205194+ivbconcept@users.noreply.github.com>
2025-10-14 06:34:04 +0200

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -513,7 +513,7 @@ export function LandingPage({ onSignUp, onSignIn, onNavigateToInvestors }: Landi
           </div>
         </section>
 
-        <section className="py-16 bg-white">
+        <section className="py-8 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="relative overflow-hidden">
               <div className="flex animate-scroll">
