818a9aed72dce0bca2613b7d68fbb9a9dbaa4143
Ivan Boole <64205194+ivbconcept@users.noreply.github.com>
2025-11-02 15:44:32 +0100

    Updated AthleteDashboard.tsx

--- src/components/Dashboard/AthleteDashboard.tsx
+++ src/components/Dashboard/AthleteDashboard.tsx
@@ -30,7 +30,7 @@ export function AthleteDashboard({ onNavigate }: AthleteDashboardProps) {
   return (
     <div className="min-h-screen bg-slate-50">
       <div className="max-w-[1200px] mx-auto px-4 md:px-8 py-6 relative">
-        <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 flex items-center justify-between bg-white rounded-2xl p-3 md:p-4 shadow-sm transition-all duration-300 w-[calc(100%-2rem)] sm:w-[calc(100%-3rem)] md:w-[calc(100%-4rem)] lg:w-[calc(100%-6rem)] xl:w-[1136px]">
+        <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 flex items-center justify-between bg-white rounded-2xl p-3 md:p-4 shadow-sm transition-all duration-300 w-[calc(100%-2rem)] sm:w-[calc(100%-3rem)] md:w-[calc(100%-4rem)] lg:w-[calc(100%-6rem)] xl:w-[calc(100%-8rem)] max-w-[1136px]">
           <div className="flex-1 max-w-[200px] md:max-w-xs lg:max-w-md">
             <div className="relative">
               <Search className="absolute left-3 md:left-4 top-1/2 -translate-y-1/2 w-4 h-4 md:w-5 md:h-5 text-slate-400" />
